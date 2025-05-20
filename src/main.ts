fb4eae2d69aa1e0bb230387cf621cddcba50803a
lazyyq <kykint@naver.com>
2025-05-21 01:39:31 +0900

    chore[#20]: setup MSW
    Mocking will be enabled when in dev mode and VITE_MOCK_API is set to any value but empty string.

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,16 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+async function prepareApp() {
+  app.use(createPinia())
+  app.use(router)
 
-app.mount('#app')
+  if (import.meta.env.DEV && import.meta.env.VITE_MOCK_API) {
+    const { worker } = await import('./mocks/browser')
+    await worker.start()
+  }
+
+  app.mount('#app')
+}
+
+prepareApp()
